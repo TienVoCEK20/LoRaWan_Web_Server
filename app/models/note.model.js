38480ca77c98dd2d64f42013e237bd44ca364323
Tien <vodangduytien@gmail.com>
2021-07-08 13:35:07 +0700

    create a schema savingdata in database

--- app/models/note.model.js
+++ app/models/note.model.js
@@ -1,10 +1,22 @@
-const mongoose = require('mongoose');
-
-const NoteSchema = mongoose.Schema({
-    title: String,
-    content: String
-}, {
-    timestamps: true
-});
-
-module.exports = mongoose.model('Note', NoteSchema);
+var mongoose=require('mongoose');
+var db = require('../database');
+// create an schema
+var userSchema = new mongoose.Schema({
+            Time: String,
+            Device ID:String,
+            Humidity:String,
+            Temperature:String
+        });
+userTable=mongoose.model('users',userSchema);
+        
+module.exports={
+     
+     fetchData:function(callback){
+        var userData=userTable.find({});
+        userData.exec(function(err, data){
+            if(err) throw err;
+            return callback(data);
+        })
+        
+     }
+}
