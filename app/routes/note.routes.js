07c2abe3795dc7f5afcf7b9d8ccd470f4aad5e66
Tien <vodangduytien@gmail.com>
2021-07-08 14:09:52 +0700

    update

--- app/routes/note.routes.js
+++ app/routes/note.routes.js
@@ -1,18 +1,5 @@
-module.exports = (app) => {
-    const notes = require('../controllers/note.controller.js');
-
-    // Create a new Note
-    app.post('/notes', notes.create);
-
-    // Retrieve all Notes
-    app.get('/notes', notes.findAll);
-
-    // Retrieve a single Note with noteId
-    app.get('/notes/:noteId', notes.findOne);
-
-    // Update a Note with noteId
-    app.put('/notes/:noteId', notes.update);
-
-    // Delete a Note with noteId
-    app.delete('/notes/:noteId', notes.delete);
-}
+var express = require('express');
+var router = express.Router();
+var userController= require('../controllers/note.controller.js');
+router.get('/fetch-data',fetchController.fetchData);
+module.exports = router;
